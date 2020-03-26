3dc1b1309a09c1f50bd3ac299ff108447119e428
Carlos Daniel <183403@ids.upchiapas.edu.mx>
2020-03-26 09:36:26 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
+import { environment } from "src/environments/environment";
+
+const config: SocketIoConfig = {
+  url: "http://3.92.137.22:3000",
+  options: {}
+};
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, DashboardComponent],
+  imports: [BrowserModule, SocketIoModule.forRoot(config)],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
